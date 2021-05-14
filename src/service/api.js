e99615595c98acbdd0c6812ae15c22ae4271247e
Abhi Shah <abhi41348@gmail.com>
2021-05-14 16:44:21 +0530

    installed serve

--- src/service/api.js
+++ src/service/api.js
@@ -1,6 +1,8 @@
 import axios from "axios";
 
-const api = axios.create({ baseURL: "http://localhost:8000/" });
+const api = axios.create({
+  baseURL: "https://sport-app-backend-apis.herokuapp.com/",
+});
 
 api.interceptors.request.use(function (config) {
   const user = localStorage.getItem("user");
